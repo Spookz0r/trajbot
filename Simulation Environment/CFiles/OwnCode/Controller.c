46d6a32be972eef8f3ac2a4a03bc06d8c5573c49
Spookz0r <gabriel.andersson@hotmail.com>
2016-03-01 13:53:39 +0100

    Added controller code

---
+++ Simulation Environment/CFiles/OwnCode/Controller.c
@@ -0,0 +1,148 @@
+
+left = 0;
+right = 0;
+
+//We only have goal position
+//Here we calculate the angle from the positions
+dy = yg-y0;
+dx = xg-x0;
+
+goaltheta = atan(dy/dx)*180/M_PI;
+if (dx==0){
+	if (dy<0){
+	goaltheta=-90;	
+	}
+	if (dy>0){
+	goaltheta=90;	
+	}
+}
+
+if(dx <  0){
+	goaltheta+=180;
+}
+
+if(goaltheta <= -180) goaltheta += 360; 
+if(goaltheta > 180) goaltheta -= 360;
+
+dy = yg-y;
+dx = xg-x;
+
+//All this mess is due to atan
+if(dx < 0){ 
+  desiredtheta = atan(dy/dx)*180/M_PI;
+  if(dy < 0){
+    desiredtheta +=180;
+  }
+  else{
+    desiredtheta -=180;
+  } 
+ }else{
+  if(dx != 0){
+    desiredtheta = atan(dy/dx)*180/M_PI;
+  }else{
+    desiredtheta = desiredtheta;
+  }
+ }
+//if(dx <  0){
+	//desiredtheta+=180;
+//}
+if (dx==0){
+	if (dy<0){
+	desiredtheta=-90;	
+	}
+	if (dy>0){
+	desiredtheta=90;	
+	}
+}
+//Our signal is limited to -180 to 180
+if(theta < -170){
+	if(desiredtheta > 180) desiredtheta = desiredtheta*-1;
+}
+else  if(theta > 170){
+	if(desiredtheta <= -180) desiredtheta = desiredtheta*-1; 	
+}
+else{
+ if(desiredtheta <= -180) desiredtheta += 360; 
+ if(desiredtheta > 180) desiredtheta -= 360;
+}
+//Controller for rotation
+K_rot = 0.5f*L/R;
+//K = 3.0f;
+u_rot = K_rot*(desiredtheta -theta);
+
+
+//Controller for translation
+K_trans = 5.0f;
+K_p = 5.0f;
+p = 5.0f;
+d0 = (cos(theta*M_PI/180) *(x0 -x) + sin(theta*M_PI/180)* (y0 -y));
+dg = (cos(goaltheta*M_PI/180) *(xg -x) + sin(goaltheta*M_PI/180)* (yg -y));
+dp = sin(goaltheta*M_PI/180)*(x+p*cos(theta*M_PI/180) -x0) - cos(goaltheta*M_PI/180)*(y+p*sin(theta*M_PI/180) -y0);
+//u_rot = dp*K_p;
+//u_trans = dg*K_trans;
+//u_trans = K_trans*d0;
+//////////////////////////////////////////  HYBRID CONTROLLER /////////////////////
+
+
+switch(state){
+	case 0:
+		//Rotate
+		//if(dy == 0 && dx < 0) desiredtheta = -180;
+		u_rot = K_rot *	(desiredtheta - theta);
+		u_trans = K_trans * d0;
+		
+		if(abs((desiredtheta -theta)) < 5 && abs(x-x0)<2 && abs(y-y0)<2){
+			state = 1;		
+		}
+		Serial.print("IN CASE 0");
+		break;
+
+	case 1:
+		//Translate
+		u_rot = K_p * dp;
+		u_trans = K_trans * dg;
+
+		if(abs((xg -x)) < 0.5 && abs((yg -y)) < 0.5){
+			state = 2;
+			old_xg = xg;
+			old_yg = yg;		
+		}
+		Serial.print("IN CASE 1\n");
+		break;
+	
+	case 2:
+		//Wait
+		u_rot = 0;
+		u_trans = 0;
+		Serial.print("IN CASE 2");
+		break;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+left += round(u_trans);
+right += round(u_trans);
+
+left += round(-u_rot);
+right +=round(u_rot);
+
+//Debugprints to MainWindow.
+Serial.print("\nOWN:");
+Serial.print(desiredtheta-theta, DEC);
+Serial.print(";");
+Serial.print(d0, DEC);
+Serial.print(";\n");
+Serial.print("goaltheta=(degree)");
+Serial.print(desiredtheta, DEC);
+Serial.print(";\n");
+Serial.print("sin theta");
+Serial.print(sin(theta), DEC);
+Serial.print(";\n");
+Serial.print("cos theta");
+Serial.print(cos(theta), DEC);
+Serial.print(";\n");
+Serial.print("xg = ");
+Serial.print(xg);
+Serial.print("\n x = ");
+Serial.print(x);
---
+++ Simulation Environment/CFiles/OwnCode/tempcontroller.c
@@ -0,0 +1,92 @@
+
+left = 0;
+right = 0;
+
+//We only have goal position
+//Here we calculate the angle from the positions
+dy = yg-y0;
+dx = xg-x0;
+
+//All this mess is due to atan
+if(dx < 0){ 
+  goaltheta = atan(dy/dx)*180/M_PI;
+  if(dy < 0){
+    goaltheta +=180;
+  }
+  else{
+    goaltheta -=180;
+  } 
+ }else{
+  if(dx != 0){
+    goaltheta = atan(dy/dx)*180/M_PI;
+  }else{
+    goaltheta = goaltheta;
+  }
+ }
+dy = yg-y;
+dx = xg-x;
+
+//All this mess is due to atan
+if(dx < 0){ 
+  desiredtheta = atan(dy/dx)*180/M_PI;
+  if(dy < 0){
+    desiredtheta +=180;
+  }
+  else{
+    desiredtheta -=180;
+  } 
+ }else{
+  if(dx != 0){
+    desiredtheta = atan(dy/dx)*180/M_PI;
+  }else{
+    desiredtheta = goaltheta;
+  }
+ }
+//Our signal is limited to -180 to 180
+if(goaltheta < -180) goaltheta += 360; 
+if(goaltheta > 180) goaltheta -= 360;
+
+
+//Controller for rotation
+K_rot = 0.7f*L/R;
+//K = 3.0f;
+//u_rot = K_rot*(goaltheta -theta);
+
+//Controller for translation
+K_trans = 5.0f;
+K_p = 5.0f;
+p = 5.0f;
+//d0 = (cos(theta*M_PI/180) *(x0 -x) + sin(theta*M_PI/180)* (y0 -y));
+//dg = (cos(goaltheta*M_PI/180) *(xg -x) + sin(goaltheta*M_PI/180)* (yg -y));
+//dp = sin(goaltheta*M_PI/180)*(x+p*cos(theta*M_PI/180) -x0) -
+//  cos(goaltheta*M_PI*180)*(y+p*sin(theta*M_PI/180) -y0);
+u_rot = dp*K_p;
+u_trans = dg*K_trans;
+//u_trans = K_trans*d0;
+left += round(u_trans);
+right += round(u_trans);
+
+left += round(-u_rot);
+right +=round(u_rot);
+
+////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////
+
+//Debugprints to MainWindow.
+Serial.print("OWN:");
+Serial.print(goaltheta-theta, DEC);
+Serial.print(";");
+Serial.print(d0, DEC);
+Serial.print(";\n");
+Serial.print("goaltheta=(degree)");
+Serial.print(goaltheta, DEC);
+Serial.print(";\n");
+Serial.print("sin theta");
+Serial.print(sin(theta), DEC);
+Serial.print(";\n");
+Serial.print("cos theta");
+Serial.print(cos(theta), DEC);
+Serial.print(";\n");
